32129b8b48dd150e1487e34172701833362e4a17
bethelbahiru <bethelbahiru@gmail.com>
2022-03-13 03:12:32 -0500

    url fixed

--- BookingServer/app.js
+++ BookingServer/app.js
@@ -4,11 +4,10 @@ const mongoose = require('mongoose')
 
 const authRouter = require('./routes/authRouter')
 
-// mongodb+srv://bethelmulat:<password>@cluster0.nk7lc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 const app = express()
 
 /* Database Connection */
-const url = 'mongodb+srv://bethelmulat:613718@cluster0.nk7lc.mongodb.net/Booking?retryWrites=true&w=majority'
+const url = ''
 mongoose.connect(url)
     .then(_ => console.log(`connected to DB`))
 
